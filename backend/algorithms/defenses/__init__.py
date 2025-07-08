a94f2dee1607fe13e6aa673d4592b3d8f94bbf34
Hydralune <haoylon@gamil.com>
2025-07-08 14:58:37 +0800

    Integrate preventive measures  ,add defense testing scripts

---
+++ backend/algorithms/defenses/__init__.py
@@ -0,0 +1,4 @@
+from .gaussian_blur import GaussianBlurDefense  # noqa: F401
+from .median_blur import MedianBlurDefense  # noqa: F401
+from .jpeg_compression import JPEGCompressionDefense  # noqa: F401
+from .bit_depth_reduction import BitDepthReductionDefense  # noqa: F401
